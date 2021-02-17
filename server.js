bcb262c3e07c7f234452b51af5a6f85cdc3dd48b
dhanush72 <dhanuraj7258@gmail.com>
2021-02-17 17:19:41 +0530

    final changes

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "2mb" }));
 
 if (process.env.NODE.ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
