d15c72b26cd0aa9973b228e230ff9ad870642677
dhanush72 <dhanuraj7258@gmail.com>
2021-02-18 12:45:41 +0530

    production build

--- server.js
+++ server.js
@@ -30,8 +30,15 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json({ limit: "2mb" }));
 
-const buildPath = path.join(__dirname, "/client", "build");
-app.use(express.static(buildPath));
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 
 // * looping routes with middlewares
 fs.readdirSync("./routes").map((route) =>
